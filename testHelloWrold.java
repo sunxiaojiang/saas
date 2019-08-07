76f9421e84471b46f9886a4c97773db64fa0c186
sunxiaojiang <sunxiao_jiang@163.com>
2019-08-07 10:14:24 +0800

    提交saas

--- testHelloWrold.java
+++ testHelloWrold.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+        System.out.println("!");
         System.out.println("HelloWorld!1");
         System.out.println("HelloWorld!2");
         System.out.println("HelloWorld!3");
