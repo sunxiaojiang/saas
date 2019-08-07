52c7ef0b7bafe77f2980bbb6ff10905d9ccd5c7f
sunxiaojiang <sunxiao_jiang@163.com>
2019-08-07 09:52:44 +0800

    tets提交

---
+++ testHelloWrold.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class Main {
+
+    @Test
+    public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!1");
+        System.out.println("HelloWorld!2");
+        System.out.println("HelloWorld!3");
+        System.out.println("HelloWorld!4");
+        System.out.println("HelloWorld!4");
+        System.out.println("HelloWorld!6");
+        System.out.println("HelloWorld!6");
+
+
+        System.out.println("HelloWorld!5");
+    }
+}
